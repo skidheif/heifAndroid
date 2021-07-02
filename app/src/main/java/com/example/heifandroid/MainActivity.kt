da33cc4c0f5da588dcdefc207f8ab0c2afb7573b
Andrey Egorov <andr.egorov@corp.mail.ru>
2021-07-02 14:19:42 +0300

    complete homework 1, for kotlin lesson 1

--- app/src/main/java/com/example/heifandroid/MainActivity.kt
+++ app/src/main/java/com/example/heifandroid/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.heifandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth_screen)
+        setContentView(R.layout.activity_main)
+
+        val randomNumber: Int = generateRandomNumber();
+        Log.d("kotlinHomeworkOne", "Random number is - $randomNumber")
+
+        val circleArea: Int = (Math.PI * (randomNumber * randomNumber)).toInt()
+        Log.d("kotlinHomeworkOne", "Circle area is - $circleArea")
+
+        val squareArea: Int = randomNumber * randomNumber
+        Log.d("kotlinHomeworkOne", "Square area is - $squareArea")
+
+        when {
+            circleArea > squareArea -> Log.d("kotlinHomeworkOne", "The area of the circle is greater than the area of the square")
+            squareArea > circleArea -> Log.d("kotlinHomeworkOne", "The area of the square is greater than the area of the circle")
+        }
+
+    }
+
+    private fun generateRandomNumber(): Int {
+        val startRandomRange: Int = 0
+        val endRandomRange: Int = 10;
+        return (startRandomRange..endRandomRange).random()
     }
 }
