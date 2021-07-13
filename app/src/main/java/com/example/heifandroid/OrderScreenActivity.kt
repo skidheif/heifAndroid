cc87783b3b807138bd00095f1494f46c4bd817e4
Andrey Egorov <andr.egorov@corp.mail.ru>
2021-07-13 21:02:19 +0300

    homework for lesson activity in kotlin done

---
+++ app/src/main/java/com/example/heifandroid/OrderScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.heifandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
+class OrderScreenActivity : AppCompatActivity() {
+
+    companion object {
+        const val PHONE_NUMBER_KEY = "PHONE_NUMBER"
+
+        fun newInstance(activity: AuthActivity, phoneNumber: String) {
+            val myIntent = Intent(activity, OrderScreenActivity::class.java)
+            myIntent.putExtra(PHONE_NUMBER_KEY, phoneNumber)
+            activity.startActivity(myIntent)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_screen)
+        val tvOrderBin = findViewById<TextView>(R.id.tvOrderBin)
+        tvOrderBin.text =
+            "Phone number from auth screen is:" + "\n" + intent.getStringExtra(PHONE_NUMBER_KEY)
+    }
+}
