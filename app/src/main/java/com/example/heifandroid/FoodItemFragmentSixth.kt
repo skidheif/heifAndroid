7e8f28e6631081b5ddad0ba23fa84557d4d105dc
Andrey Egorov <andr.egorov@corp.mail.ru>
2021-08-03 16:32:52 +0300

    homework for lesson about toolbar complete

---
+++ app/src/main/java/com/example/heifandroid/FoodItemFragmentSixth.kt
@@ -0,0 +1,18 @@
+package com.example.heifandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FoodItemFragmentSixth : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.food_item_fragment_sixth, container, false)
+    }
+}
--- app/src/main/java/com/example/heifandroid/MainActivity.kt
+++ app/src/main/java/com/example/heifandroid/MainActivity.kt
@@ -1,27 +1,65 @@
 package com.example.heifandroid
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var vpMainFirst: ViewPager2
+    lateinit var vpMainSecond: ViewPager2
+    lateinit var btvMain: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val vpMain = findViewById<ViewPager2>(R.id.vpMain)
-        vpMain.adapter = ViewPagerAdapter(this).apply {
+        vpMainFirst = findViewById(R.id.vpMainFirst)
+        vpMainSecond = findViewById(R.id.vpMainSecond)
+        btvMain = findViewById(R.id.btvMain)
+
+        vpMainFirst.adapter = ViewPagerAdapter(this).apply {
             addFoodFragment(FoodItemFragmentFirst())
             addFoodFragment(FoodItemFragmentSecond())
             addFoodFragment(FoodItemFragmentThird())
         }
+        vpMainFirst.isUserInputEnabled = false
+
+        vpMainSecond.adapter = ViewPagerAdapter(this).apply {
+            addFoodFragment(FoodItemFragmentFourth())
+            addFoodFragment(FoodItemFragmentFifth())
+            addFoodFragment(FoodItemFragmentSixth())
+        }
+        vpMainSecond.isUserInputEnabled = false
 
         val foodTabNames = listOf("Всё", "Фаст-фуд", "Десерты")
         val tbMain = findViewById<TabLayout>(R.id.tbMain)
-        TabLayoutMediator(tbMain, vpMain) { tab, position ->
+
+        TabLayoutMediator(tbMain, vpMainFirst) { tab, position ->
             tab.text = foodTabNames[position]
         }.attach()
+
+        TabLayoutMediator(tbMain, vpMainSecond) { tab, position ->
+            tab.text = foodTabNames[position]
+        }.attach()
+
+        btvMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.itMenu -> {
+                    vpMainFirst.visibility = View.VISIBLE
+                    vpMainSecond.visibility = View.INVISIBLE;
+                    vpMainFirst.currentItem = 0
+                }
+                R.id.itFavourite -> {
+                    vpMainSecond.currentItem = 0
+                    vpMainFirst.visibility = View.INVISIBLE
+                    vpMainSecond.visibility = View.VISIBLE;
+                }
+            }
+            true
+        }
     }
 }
